b99d679c45c8400908edd6e1b595cdbb521e6557
Prathak Paisanwatcharakit <lpaydat@gmail.com>
2024-11-04 04:05:10 +0700

    add houdini

---
+++ website/tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss';
+
+import { skeleton } from "@skeletonlabs/tw-plugin";
+
+const config = {
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/@skeletonlabs/skeleton/**/*.{html,js,svelte,ts}'
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: [
+		skeleton({
+			themes: { preset: ['crimson'] }
+		})
+	]
+} satisfies Config;
+
+export default config;
