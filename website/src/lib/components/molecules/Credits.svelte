6045cfd46b4a11a47ef2184669dd3ba9631bdd57
Prathak Paisanwatcharakit <lpaydat@gmail.com>
2024-12-06 23:16:22 +0700

    improve styles

--- website/src/lib/components/molecules/Credits.svelte
+++ website/src/lib/components/molecules/Credits.svelte
@@ -1,5 +1,5 @@
 <div
-	class="fixed bottom-0 right-0 flex w-full md:w-fit justify-between rounded-md bg-black/40 px-2 md:px-4 py-1 md:py-2 text-xs text-surface-300/80"
+	class="fixed bottom-0 right-0 flex w-full md:w-fit justify-between rounded-md bg-black/40 px-2 lg:px-4 py-1 lg:py-2 text-xs text-surface-300/80"
 >
 	<div class="flex-1 text-right">v1.3.0 | made with ❤️ lpaydat</div>
 </div>
