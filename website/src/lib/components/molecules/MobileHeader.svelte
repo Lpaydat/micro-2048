9b540a14f3ae95f51d396077ff97b775916f763c
lpaydat <lpaydat@gmail.com>
2025-02-08 04:26:50 +0700

    add version label to mobile logo

--- website/src/lib/components/molecules/MobileHeader.svelte
+++ website/src/lib/components/molecules/MobileHeader.svelte
@@ -6,8 +6,13 @@
 
 <div class="m-2 flex items-center justify-between rounded-lg bg-[#faf8ef] px-4 py-2 shadow-sm">
 	<a href="/">
-		<div class="flex-none">
+		<div class="relative flex-none">
 			<BrandLogo />
+			<span
+				class="font-handwriting text-md absolute -bottom-1 left-8 z-10 -rotate-12 transform text-red-500 opacity-80 drop-shadow-[0_1px_1px_rgba(0,0,0,0.25)]"
+			>
+				Archimedes
+			</span>
 		</div>
 	</a>
 
@@ -15,3 +20,12 @@
 		{@render children?.()}
 	</div>
 </div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&display=swap');
+
+	.font-handwriting {
+		font-family: 'Dancing Script', cursive;
+		text-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	}
+</style>
